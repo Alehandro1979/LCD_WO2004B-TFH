d57f6847c17d64e91f5fca7d5543f5539d1d23ce
Alexander <alexander.antipov79@gmail.com>
2026-03-25 11:14:45 +0300

    fix

--- examples/WO2004A_stm32f4/WO2004A_stm32f4.ino
+++ examples/WO2004A_stm32f4/WO2004A_stm32f4.ino
@@ -23,7 +23,7 @@ void setup()
   // Wire.begin(SDA_pin,SCL_pin); // using pin esp32
   // Wire.setClock (1000000);
 
-  // TEST
+  // TEST LCD
   lcd.init();
   lcd.rotateOn();
   lcd.shiftStartLine(32);
